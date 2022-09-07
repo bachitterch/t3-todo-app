6745536fd841fbc43f7415e2dabf0a57873f4e3b
Bachitter Chahal <officialbachitterchahal@gmail.com>
2022-09-07 03:13:29 -0700

    Created todo create and query functions

---
+++ src/server/router/todo.ts
@@ -0,0 +1,46 @@
+import { z } from 'zod'
+import { createRouter } from './context'
+import * as trpc from '@trpc/server';
+
+export const todoRouter = createRouter()
+.query('getTodos', {
+  async resolve({ ctx }) {
+      try {
+        return await ctx.prisma.todo.findMany({
+          where: {
+            userId: ctx.session?.user?.id as string
+          },
+          orderBy: {
+            createdAt: "desc",
+          },
+        });
+      } catch (error) {
+        console.log("error", error);
+      }
+  },
+})
+.middleware(async({ctx, next}) => {
+  if (!ctx.session) {
+      throw new trpc.TRPCError({ code: "UNAUTHORIZED" });
+    }
+    return next();
+})
+.mutation("createTodo", {
+  input: z.object({
+    todo: z.string(),
+    completed: z.boolean()
+  }),
+  async resolve({ ctx, input }) {
+    try {
+      await ctx.prisma.todo.create({
+        data: {
+          todo: input.todo,
+          completed: input.completed,
+          userId: ctx.session?.user?.id as string
+        }
+      })
+    } catch(error) {
+      console.error(error)
+    }
+  }
+})
